a70078fb29369a360c48a83010f6e40bc0c0f9b4
carlos alonso barahona alvarez <carlosalonsobarahona261@gmail.com>
2024-02-22 14:39:30 -0500

    actualizacion2

---
+++ uniquindio_workspace/mi_proyecto/src/main/java/co/edu/uniquindio/poo/App.java
@@ -0,0 +1,17 @@
+package co.edu.uniquindio.poo;
+
+public class App {
+    public static void main(String[] args) {
+        
+        Perro nani = new Perro("camilo", "husky");
+
+       
+
+        for(int i = 0;i < 3; i++){
+
+            nani.caminar(30);
+        }
+
+    }
+
+}
